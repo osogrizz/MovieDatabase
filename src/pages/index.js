4366ee88617e7442fdc577299122e14bc813b42a
osogrizz <lhtor@gmail.com>
2019-05-30 11:21:03 -0700

    Added responsiveness to controls.

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,18 +5,21 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import styled from 'styled-components'
 import Movie from '../components/movie';
-import { MdSkipPrevious, MdSkipNext } from 'react-icons/md'
+import { MdSkipPrevious, MdSkipNext, MdPlayArrow } from 'react-icons/md'
 
 const MovieGrid = styled.div`
   font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
     "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
     sans-serif;
   padding: 4rem;
+  /* max-width: 1240px;
+  margin: 0 auto; */
   display: grid;
-  grid-gap: 30px;
+  grid-gap:  0 20px;
   grid-template-columns: repeat(5, 1fr);
   justify-items: center;
   align-items: center;
+  text-align: center;
   
   @media (max-width: 800px) {
     grid-template-columns: repeat(3, 1fr);
@@ -33,13 +36,23 @@ const MovieGrid = styled.div`
   `
 
 const PageControls = styled.div`
+font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+    "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+    sans-serif;
   color: #fff;
   margin: 80px 0;
   justify-items: center;
   justify-content: center;
   text-align: center;
+  align-items: center;
   display: grid;
   grid-template-columns: repeat(5, 1fr);
+
+   div {
+     font-size: 30px;
+     font-weight: 300;
+     letter-spacing: 2px;
+   }
   
   button {
     font-size: 50px;
@@ -49,6 +62,25 @@ const PageControls = styled.div`
     outline: none;
     cursor: pointer;
     width: 30px;
+  }
+  #prev {
+    transform: scaleX(-1);
+  }
+  @media (max-width: 800px) {
+    button {
+      font-size: 30px
+    }
+    div {
+      font-size: 20px
+    }
+  }
+  @media (max-width: 600px) {
+    button {
+      font-size: 25px
+    }
+    div {
+      font-size: 16px;
+    }
   }
 `
 
@@ -92,12 +124,12 @@ const IndexPage = ({props}) => {
     }
   }
   
-  // API call extracted to named function making it available to 'setting' by Hooks.
+  // API call extracted to named function making it available to be 'set' by Hooks.
   const fetchData = async () => {
     try {
       const res = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=6d31c18d73745e3328f88183fb494647&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}`)
       const movies = await res.json()
-      console.log('fetched', movies)
+      // console.log('fetched', movies)
       setMovies(movies.results)
     } catch(e) {
       console.log(e)
@@ -112,18 +144,21 @@ const IndexPage = ({props}) => {
   return (
     <Layout>
       <SEO title="Home" />
+
       <MovieGrid>
         {movies.map(movie => (
           <Movie key={movie.id} src={movie.id} alt={movie.id} movie={movie} />
         ))}
       </MovieGrid>
+
       <PageControls>
-        <button onClick={() => resetHandler()}> <span> <MdSkipPrevious /></span> </button>
-        <button onClick={() => prevHandler()} ><span>←</span></button>
+        <button onClick={() => resetHandler()}> <span><MdSkipPrevious /></span></button>
+        <button id="prev" onClick={() => prevHandler()} ><span><MdPlayArrow /></span></button>
         <div>Page {page}</div>
-        <button onClick={() => nextHandler()} ><span>→</span></button>
+        <button onClick={() => nextHandler()} ><span><MdPlayArrow /></span></button>
         <button onClick={() => lastHandler()} ><span><MdSkipNext /></span></button>
       </PageControls>`
+
     </Layout>
   )
 }

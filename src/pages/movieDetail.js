0c1d36f9866166b50b71e0e7846f3a0983cbc170
osogrizz <lhtor@gmail.com>
2019-05-31 17:19:21 -0700

    Added styles to detail page.

--- src/pages/movieDetail.js
+++ src/pages/movieDetail.js
@@ -14,6 +14,26 @@ const MovieWrapper = styled.div`
   background: url(${props => props.backdrop}) no-repeat;
   background-size: cover;
 `
+const Blob = styled.div`
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+    "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+    sans-serif;
+  display: grid;
+  text-align: left;
+  grid-template-rows: 80px;
+
+  p {
+    padding: 20px;
+  }
+
+  #desc {
+    width: 960px;
+    font-size: 1.2rem;
+    letter-spacing: 1px;
+    line-height: 32px;
+  }
+
+`
 
 const MovieDetail = ({ location }) => {
   const [movie, setMovie] = useState({})
@@ -41,8 +61,10 @@ const MovieDetail = ({ location }) => {
 
       <div style={{ display: `flex`,background: `#fff` }}>
         <Poster style={{ margin: `-5rem 60px 220px 60px` }} src={`${POSTER_PATH}${movie.poster_path}`} alt={`${movie.title}`} />
-        <p>{movie.release_date}</p>
-        <p>{movie.overview}</p>
+        <Blob>
+          <p>Release Date: {movie.release_date}</p>
+          <p id="desc" >{movie.overview}</p>
+        </Blob> 
       </div>
       </MovieWrapper>
     </Layout>
